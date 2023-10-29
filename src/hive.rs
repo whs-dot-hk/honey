b6f804f1e3b2a171afdf88c5c4dca8c82d4709ed
whs <hswongac@gmail.com>
2023-10-29 22:06:58 +0800

    Add configuration type

--- src/hive.rs
+++ src/hive.rs
@@ -2,6 +2,7 @@ pub mod configurations;
 pub mod import;
 pub mod inherit;
 
+pub use crate::hive::configurations::ConfigurationType;
 pub use crate::hive::configurations::Configurations;
 pub use crate::hive::import::Import;
 pub use crate::hive::import::Imports;
--- src/hive/configurations.rs
+++ src/hive/configurations.rs
@@ -1,8 +1,54 @@
 use crate::hive::*;
 use genco::prelude::*;
 
+pub enum ConfigurationType {
+    Import(Import),
+}
+
 pub struct Configurations {
     pub imports: Imports,
+}
+
+impl Configurations {
+    /// ```
+    /// use genco::prelude::*;
+    /// use honey::hive::*;
+    ///
+    /// let configurations = Configurations::new(vec![
+    ///     ConfigurationType::Import(Import::disko()),
+    ///     ConfigurationType::Import(Import::disko_configurations("my-disko-configurations")),
+    /// ]);
+    ///
+    /// let toks = quote!($configurations);
+    ///
+    /// assert_eq!(
+    ///     vec![
+    ///         "let",
+    ///         "    inherit (inputs) disko;",
+    ///         "in",
+    ///         "",
+    ///         "{",
+    ///         "    imports = [",
+    ///         "        disko.nixosModules.disko",
+    ///         "        cell.diskoConfigurations.my-disko-configurations",
+    ///         "    ];",
+    ///         "}"
+    ///     ],
+    ///     toks.to_file_vec()?
+    /// );
+    /// # Ok::<_, genco::fmt::Error>(())
+    /// ```
+    pub fn new(configurations: Vec<ConfigurationType>) -> Self {
+        let mut imports = Vec::new();
+        for c in configurations {
+            match c {
+                ConfigurationType::Import(import) => imports.push(import),
+            }
+        }
+        Self {
+            imports: Imports(imports),
+        }
+    }
 }
 
 impl FormatInto<Nix> for Configurations {
