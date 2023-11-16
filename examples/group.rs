de4adc33744936e41ec1262f281f4f0a4912512a
whs <hswongac@gmail.com>
2023-11-16 13:55:23 +0800

    Update example

--- examples/group.rs
+++ examples/group.rs
@@ -2,14 +2,14 @@ use genco::fmt;
 use genco::prelude::*;
 use honey::hive::*;
 
-fn template(name: &str) -> nix::Tokens {
+fn template(prefix: &str, name: &str, number: u32) -> nix::Tokens {
     let group = &nix::inherit("inputs.cells", "group");
 
     let mut tokens = nix::Tokens::new();
     tokens.append("{");
     tokens.indent();
-    for n in 0..2 {
-        let machine_name = &format!("machine{:02}", n);
+    for n in 0..number {
+        let machine_name = &format!("{}{:02}", prefix, n);
         quote_in! { tokens =>
             $machine_name = {
                 imports = [
@@ -25,15 +25,18 @@ fn template(name: &str) -> nix::Tokens {
 }
 
 fn main() -> anyhow::Result<()> {
+    let number = 2;
+    let prefix = "machine";
+
     let nixos_configurations =
-        NixosConfigurations::new1("machine", 2, None, "nixos-23-05", "aarch64-linux");
+        NixosConfigurations::new1(prefix, number, None, "nixos-23-05", "aarch64-linux");
 
     let tokens = quote!($nixos_configurations);
 
-    let disko_configurations = template("diskoConfigurations");
-    let hardware_profiles = template("hardwareProfiles");
-    let nixos_modules = template("nixosModules");
-    let nixos_profiles = template("nixosProfiles");
+    let disko_configurations = template(prefix, "diskoConfigurations", number);
+    let hardware_profiles = template(prefix, "hardwareProfiles", number);
+    let nixos_modules = template(prefix, "nixosModules", number);
+    let nixos_profiles = template(prefix, "nixosProfiles", number);
 
     let tokens2 = quote!($disko_configurations);
     let tokens3 = quote!($hardware_profiles);
